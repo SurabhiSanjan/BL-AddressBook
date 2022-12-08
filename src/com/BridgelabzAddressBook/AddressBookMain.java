62bc8bae2b27b2805e22d715ddace0766a58be8c
surabhisanjan <surabhisanjan@gmail.com>
2022-12-08 10:18:59 +0530

    Addition: adding Contact to address Book

--- src/com/BridgelabzAddressBook/AddressBookMain.java
+++ src/com/BridgelabzAddressBook/AddressBookMain.java
@@ -1,7 +1,12 @@
 package com.BridgelabzAddressBook;
-
+//Ability to create a Contacts in Address
+//Book with first and last names, address,
+//city, state, zip, phone number and
+//email...
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        Contact cd = new Contact();
+        System.out.println(cd);
     }
 }
